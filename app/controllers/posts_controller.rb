d09d9e53fd73f3a0a19d04e06b740dbeb30d7fba
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-03-09 16:15:09 -0500

    ADD: Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,22 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new
+    @post.user_id = currrent_user.id
+    
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  
+
 end
