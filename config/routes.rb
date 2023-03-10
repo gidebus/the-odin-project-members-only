28e8926588f6e1064d7fdfcb707e9e864e2868c2
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-03-10 08:30:16 -0500

    ADD: fix sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => 'users/registrations' }
 
-  devise_scope :users do
+  devise_scope :user do
     get 'register', to: 'users/registrations#new', as: :signup
+    get '/users/sign_out', to: 'users/sessions#destroy'
   end
 
   resources :posts, only: [:new, :create, :index]
